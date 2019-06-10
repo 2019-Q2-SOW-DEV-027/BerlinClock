4be3fc0df9a3a3003d5f86b480fd96061f16db26
2019-Q2-SOW-DEV-027 <2019Q2SOWDEV027@gmail.com>
2019-06-11 04:48:01 +0530

    test: Initialize digital time before all test

--- spec/model/digitalTimeSpec.js
+++ spec/model/digitalTimeSpec.js
@@ -1,15 +1,18 @@
 const DigitalTime = require("../../src/model/digitalTime");
 
 describe("Digital Time Model", function () {
+    let digitalTime;
+    beforeAll(function(){
+        digitalTime = new DigitalTime();
+    });
+
     it("should notify when minutes is invalid", function () {
-        let digitalTime = new DigitalTime();
         digitalTime.minutes = "";
 
         expect(digitalTime.isInvalid()).toBe(true);
     });
 
     it("should notify when hours is invalid", function () {
-        let digitalTime = new DigitalTime();
         digitalTime.minutes = 50;
         digitalTime.hours = "";
 
@@ -17,7 +20,6 @@ describe("Digital Time Model", function () {
     });
 
     it("should notify error when seconds is invalid", function () {
-        let digitalTime = new DigitalTime();
         digitalTime.minutes = 50;
         digitalTime.hours = 10;
         digitalTime.seconds = "";
