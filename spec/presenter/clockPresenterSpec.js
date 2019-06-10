b720bec0b4109fff6d37a47f69ec06fc7b291225
2019-Q2-SOW-DEV-027 <2019Q2SOWDEV027@gmail.com>
2019-06-11 01:25:53 +0530

    test: Declare digital time, clock presenter and view as common

--- spec/presenter/clockPresenterSpec.js
+++ spec/presenter/clockPresenterSpec.js
@@ -1,35 +1,35 @@
 const ClockPresenter = require("../../src/presenter/clockPresenter");
 
 describe("Berlin Clock Presenter", function () {
+    let digitalTime = {};
+    let clockView = {};
+    let clockPresenter;
+
     it("should display error message when time is invalid", function () {
-        let digitalTime = {};
         digitalTime.isInvalid = function(){
             return true;
         };
-        let clockView = {};
         clockView.displayError = function(){};
         spyOn(clockView, "displayError");
-        let clockPresenter = new ClockPresenter(digitalTime, clockView);
+        clockPresenter = new ClockPresenter(digitalTime, clockView);
 
         clockPresenter.convertToBerlinTime();
 
         expect(clockView.displayError).toHaveBeenCalled();
     });
 
-    describe("Lower Minutes Row", function(){
+    describe("LOWER MINUTES ROW", function(){
 
         it("should display OOOO at minutes 0", function () {
-            let digitalTime = {};
             digitalTime.isInvalid = function(){
                 return false;
             };
             digitalTime.minutes = 0;
-            let clockView = {};
             clockView.displayCurrentTimeWithTitles = function(){};
             clockView.displayLamps = function(){};
             spyOn(clockView, "displayCurrentTimeWithTitles");
             spyOn(clockView, "displayLamps");
-            let clockPresenter = new ClockPresenter(digitalTime, clockView);
+            clockPresenter = new ClockPresenter(digitalTime, clockView);
 
             clockPresenter.convertToBerlinTime();
 
